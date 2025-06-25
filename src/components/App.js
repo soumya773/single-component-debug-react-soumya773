7adef1fe401f551185192f5c10bc759f7901b2b7
soumya773 <jenasoumya2002@gmail.com>
2025-06-25 09:37:05 +0530

    debuging single comp.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,15 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// App.js
+import React from 'react';
+
+// Functional component
+function App() {
+  return (
+    <div id="main">
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+}
+
+export default App;
+
 
